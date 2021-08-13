6e526351c23948079384c7e50403fc0121db6594
anas1503 <md.anas1503@gmail.com>
2021-08-13 22:07:16 +0530

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 import DiceRoll from './components/diceRoll/DiceRoll'
 import Header from './components/Header'
+import Dices from './assets/dices.png'
 import './App.css';
 
 const App = () => {
+  const [gamestarted, setGamestarted] = useState(false)
+
+  const starthandler = () => {
+    setGamestarted(true);
+  }
   return (
-    <div className="main">
-      <div className="wrapper">
-        <Header />
-        <DiceRoll />
-      </div>
-    </div>
+    <>
+      {
+        gamestarted ? (<div className="main">
+          <div className="wrapper">
+            <Header />
+            <DiceRoll />
+          </div>
+        </div>) : (<div className="Landing">
+          <img className="img" src={Dices} alt="dices" />
+          <div className="start">
+            <p className="title">The Dice Game</p>
+            <button className="btn1" onClick={starthandler}>Play Now</button>
+          </div>
+        </div>)
+      }
+    </>
+
+
+
   )
 }
 
